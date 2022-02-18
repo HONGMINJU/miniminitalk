831eab13eaf80e8d13d57fbe252c6c63cd025c9c
HONGMINJU <mhong0800@gmail.com>
2022-02-19 00:15:35 +0900

    bonus 추가

---
+++ bonus/minitalk_bonus.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhong  <mhong@student.42.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 00:11:27 by mhong             #+#    #+#             */
+/*   Updated: 2022/02/19 00:11:27 by mhong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+#include <signal.h>
+# include "../ft_printf/ft_printf.h"
+
+#endif
