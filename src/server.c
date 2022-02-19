f2963b5ebe846e1f1e16786fcc5695551112558b
HONGMINJU <mhong0800@gmail.com>
2022-02-19 22:33:47 +0900

    test

--- src/server.c
+++ src/server.c
@@ -25,7 +25,7 @@ void	handleSignal(int signo, siginfo_t *siginfo, void *unused)
 	digit++;
 	if (digit == 8)
 	{
-		if (msg == 0)
+		if (msg == "\0")
 			kill(siginfo->si_pid, SIGUSR2);
 		ft_printf("%c", msg);
 		digit = 0;
